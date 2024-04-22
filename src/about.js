829827d922aa7cfc16c776f32ae424510d903227
Jordan Jonathan Lin <jordanjlin2003@gmail.com>
2024-04-21 21:08:46 -0700

    changed about section

--- src/about.js
+++ src/about.js
@@ -6,7 +6,12 @@ function About() {
       <section className="about-section section-fade">
         <h2>About Me</h2>
         <p>
-          Welcome to my space on the web! I have recently completed my B.S. in Computer Science & Engineering with a minor Geospatial Information Systems and Technologie at UCLA. I specialize in IT support, crafting innovative software solutions, and developing responsive websites. My journey has been a blend of technical prowess and creative problem-solving, and I'm always eager to take on new challenges. Dive in to discover how I blend analytical skills with technological artistry to navigate and shape the digital landscape!
+          Welcome to my space on the web! I recently completed my B.S. in Computer Science & 
+          Engineering, with a minor in Geospatial Information Systems and Technologies, at UCLA. My professional
+          experience includes roles at both Esri and UCLA, where I fulfilled four internships and part-time positions 
+          in technical support and software/website development. I have thoroughly enjoyed learning how to apply my technical
+          skill set in a variety of professional environments and am always eager to take on new challenges. Dive in to discover how I 
+          blend analytical skills with technological artistry to navigate and shape the digital landscape!
         </p>
       </section>
     </>
